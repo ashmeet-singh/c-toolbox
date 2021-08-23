8ba386c9f23652c0e90e6f9f544202db165239d8
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2021-08-24 02:16:13 +0530

    Added output block

--- ChaCha20/ChaCha20.c
+++ ChaCha20/ChaCha20.c
@@ -24,19 +24,23 @@ void chacha20_quarter_round(uint32_t *v0, uint32_t *v1, uint32_t *v2, uint32_t *
     *v3 = d;
 }
 
-void process_chacha20_block(uint32_t input_block[16])
+void process_chacha20_block(uint32_t input_block[16], uint32_t output_block[16])
 {
     int i;
-    for (i = 0; i < 20; i = i + 2)
+    for (i = 0; i < 16; i++)
     {
-        chacha20_quarter_round(&input_block[0], &input_block[4], &input_block[8], &input_block[12]);
-        chacha20_quarter_round(&input_block[1], &input_block[5], &input_block[9], &input_block[13]);
-        chacha20_quarter_round(&input_block[2], &input_block[6], &input_block[10], &input_block[14]);
-        chacha20_quarter_round(&input_block[3], &input_block[7], &input_block[11], &input_block[15]);
-        chacha20_quarter_round(&input_block[0], &input_block[5], &input_block[10], &input_block[15]);
-        chacha20_quarter_round(&input_block[1], &input_block[6], &input_block[11], &input_block[12]);
-        chacha20_quarter_round(&input_block[2], &input_block[7], &input_block[8], &input_block[13]);
-        chacha20_quarter_round(&input_block[3], &input_block[4], &input_block[9], &input_block[14]);
+        output_block[i] = input_block[i];
+    }
+    for (i = 0; i < 10; i++)
+    {
+        chacha20_quarter_round(&output_block[0], &output_block[4], &output_block[8], &output_block[12]);
+        chacha20_quarter_round(&output_block[1], &output_block[5], &output_block[9], &output_block[13]);
+        chacha20_quarter_round(&output_block[2], &output_block[6], &output_block[10], &output_block[14]);
+        chacha20_quarter_round(&output_block[3], &output_block[7], &output_block[11], &output_block[15]);
+        chacha20_quarter_round(&output_block[0], &output_block[5], &output_block[10], &output_block[15]);
+        chacha20_quarter_round(&output_block[1], &output_block[6], &output_block[11], &output_block[12]);
+        chacha20_quarter_round(&output_block[2], &output_block[7], &output_block[8], &output_block[13]);
+        chacha20_quarter_round(&output_block[3], &output_block[4], &output_block[9], &output_block[14]);
     }
 }
 
@@ -44,13 +48,13 @@ uint8_t test_chacha20_block_function()
 {
     uint32_t test_input_block[16] = {0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c, 0x00000001, 0x09000000, 0x4a000000, 0x00000000};
     uint32_t test_output_block[16] = {0x837778ab, 0xe238d763, 0xa67ae21e, 0x5950bb2f, 0xc4f2d0c7, 0xfc62bb2f, 0x8fa018fc, 0x3f5ec7b7, 0x335271c2, 0xf29489f3, 0xeabda8fc, 0x82e46ebd, 0xd19c12b4, 0xb04e16de, 0x9e83d0cb, 0x4e3c50a2};
-
-    process_chacha20_block(test_input_block);
+    uint32_t output_block[16];
+    process_chacha20_block(test_input_block, output_block);
 
     int i;
     for (i = 0; i < 16; i++)
     {
-        if (test_input_block[i] != test_output_block[i])
+        if (output_block[i] != test_output_block[i])
         {
             return 0;
         }
