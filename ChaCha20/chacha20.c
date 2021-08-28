1683a616192fee91c5e35ec9087f939d40e0c6f1
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2021-08-28 14:05:52 +0530

    Refined code

--- ChaCha20/chacha20.c
+++ ChaCha20/chacha20.c
@@ -24,7 +24,7 @@ void chacha20_quarter_round(uint32_t *v0, uint32_t *v1, uint32_t *v2, uint32_t *
     *v3 = d;
 }
 
-void process_chacha20_block(uint32_t input_block[16], uint32_t output_block[16])
+void chacha20_process_block(uint32_t input_block[16], uint32_t output_block[16])
 {
     int i;
     for (i = 0; i < 16; i++)
@@ -48,12 +48,12 @@ void process_chacha20_block(uint32_t input_block[16], uint32_t output_block[16])
     }
 }
 
-uint8_t test_chacha20_block_function()
+uint8_t chacha20_test()
 {
     uint32_t test_input_block[16] = {0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c, 0x00000001, 0x09000000, 0x4a000000, 0x00000000};
     uint32_t test_output_block[16] = {0xe4e7f110, 0x15593bd1, 0x1fdd0f50, 0xc47120a3, 0xc7f4d1c7, 0x0368c033, 0x9aaa2204, 0x4e6cd4c3, 0x466482d2, 0x09aa9f07, 0x05d7c214, 0xa2028bd9, 0xd19c12b5, 0xb94e16de, 0xe883d0cb, 0x4e3c50a2};
     uint32_t output_block[16];
-    process_chacha20_block(test_input_block, output_block);
+    chacha20_process_block(test_input_block, output_block);
 
     int i;
     for (i = 0; i < 16; i++)
