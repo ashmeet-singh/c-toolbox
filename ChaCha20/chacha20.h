68ed11b7c1b95ad63857a1cd557ad54e3476a85e
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2021-10-20 22:31:21 +0530

    Bug fix

--- ChaCha20/chacha20.h
+++ ChaCha20/chacha20.h
@@ -1,6 +1,8 @@
 #ifndef _Z_CHACHA20_H_INCLUDED_
 #define _Z_CHACHA20_H_INCLUDED_
 
+#include <stdint.h>
+
 void chacha20_quarter_round(uint32_t *v0, uint32_t *v1, uint32_t *v2, uint32_t *v3);
 void chacha20_process_block(uint32_t input_block[16], uint32_t output_block[16]);
 uint_fast8_t chacha20_test();
