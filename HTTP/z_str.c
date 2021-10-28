c6abf14a4625094e08b40c6f4bc41410f0fd6f62
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2021-10-28 13:31:11 +0530

    Added trim function

--- HTTP/z_str.c
+++ HTTP/z_str.c
@@ -16,3 +16,23 @@ int64_t z_str_find_CRLF(void *buf, uint64_t len)
     }
     return -1;
 }
+
+void z_str_trim(void *buf, uint64_t len) {
+    uint8_t *s;
+    s = (uint8_t *)buf;
+
+    uint64_t i;
+    uint64_t l;
+    uint64_t e;
+    l = 0;
+    e = 0;
+    for (i = 0; i < len; i++) {
+        if (s[i] == '\0') { break; }
+        if (l != 0 || s[i] != 32) {
+            s[l] = s[i];
+            l++;
+            if (s[i] != 32) { e = l; }
+        }
+    }
+    for (i = e; i < l; i++) { s[i] = '\0'; }
+}
